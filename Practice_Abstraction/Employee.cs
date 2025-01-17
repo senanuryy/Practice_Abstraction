677be404637f1cf0ed30d7ad8707c4ac3558b031
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-17 17:40:43 +0300

    Add project files.

---
+++ Practice_Abstraction/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Abstraction
+{
+    public abstract class Employee
+    {        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+        public abstract void Task();
+    }
+
+    class ProjectManager : Employee
+    {
+        public override void Task()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is working as a {Department}. I am managing projects.");
+        }
+    }
+
+    class SoftwareDeveloper : Employee
+    {
+        public override void Task()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is working as a {Department}. I am developing software.");
+        }
+    }
+
+    class SalesRepresentative : Employee
+    {
+        public override void Task()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is working as a {Department}. I am handling client sales.");
+        }
+    }
+}
---
+++ Practice_Abstraction/Program.cs
@@ -0,0 +1,26 @@
+﻿using Practice_Abstraction;
+
+Employee employee1 = new ProjectManager()
+{
+    FirstName = "Hasan",
+    LastName = "Çıldırmış",
+    Department = "Project Manager"
+};
+
+Employee employee2 = new SoftwareDeveloper()
+{
+    FirstName = "Emily",
+    LastName = "Smith",
+    Department = "Software Developer"
+};
+
+Employee employee3 = new SalesRepresentative()
+{
+    FirstName = "John",
+    LastName = "Doe",
+    Department = "Sales Representative"
+};
+
+employee1.Task();
+employee2.Task();
+employee3.Task();
